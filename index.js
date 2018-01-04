d07600e506363776400d33649c4d1763a59212d2
Souloinc87 <souloinc87@gmail.com>
2018-01-04 02:49:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ object[key] = value
 return object
 }
 function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object)
+  var newObj = Object.assign(object, key)
   delete newObj.key
   return newObj
 }
