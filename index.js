226468a637169651dc2e82ee2a5c4c47c8da049f
Souloinc87 <souloinc87@gmail.com>
2018-01-04 00:56:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ function updateObjectWithKeyAndValue(object, key, value) {
   var obj = {prop: 1, prop2: 2}
   return obj
 }
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+obj.prop = {prop2}
+return obj
+}
